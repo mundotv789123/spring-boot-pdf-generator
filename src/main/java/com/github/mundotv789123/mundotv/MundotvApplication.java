410ba13d896ea1bd7eaaedbcdccf44f975af74cb
mundotv789123 <789michaelfernandes789@gmail.com>
2024-06-10 18:52:49 -0300

    init

---
+++ src/main/java/com/github/mundotv789123/mundotv/MundotvApplication.java
@@ -0,0 +1,13 @@
+package com.github.mundotv789123.mundotv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MundotvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MundotvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/mundotv789123/mundotv/MundotvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mundotv789123.mundotv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MundotvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
